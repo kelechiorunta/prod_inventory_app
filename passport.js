136e3a7c6471a851c3ff036bd355c7293eb00585
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-05-21 17:09:49 +0100

    feat: Google OAuth passport strategy logic

--- passport.js
+++ passport.js
@@ -1,8 +1,9 @@
 import { Strategy as LocalStrategy } from "passport-local";
 import User from "./models/User.js";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 // import passport from 'passport'
 
-const configurePassport = (passport) => {
+export const configureLocalPassport = (passport) => {
     passport.use(new LocalStrategy(
         {
             usernameField: 'username',
@@ -45,4 +46,63 @@ const configurePassport = (passport) => {
 
 }
 
-export {configurePassport}
+export const configureGooglePassport = (passport) => {
+    passport.use(new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: 'http://localhost:3301/oauth2/redirect/google',
+            scope: ['profile', 'email']
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            const newUser = {
+                "google.name": profile.displayName, // Use displayName for the name
+                "google.email": profile.emails[0].value, // Use emails[0].value for email
+                "google.accessToken": accessToken,  // ✅ save token   
+                active: true,
+            };
+            const googleUser = await User.findOne({ 'google.email': profile.emails[0].value })
+            const localUser = await User.findOne({ email: profile.emails[0].value })
+    
+            try {
+                if (googleUser && localUser) {
+                    return done(null, googleUser)
+                } else if (googleUser) {
+                    return done(null, googleUser)
+                } else if (localUser) {
+                    localUser.google = {
+                        email: profile.emails[0].value,
+                        displayName: profile.displayName,
+                        accessToken
+                    }
+                    await localUser.save();
+                    console.log('Local user updated with Google info');
+                    done(null, localUser)
+                }
+                else { //User is new
+                    const user = User.create(newUser)
+                    done(null, user)
+                }
+            }
+            catch (err) {
+                console.error("Google authentication error:", err.message)
+                done(err, null)
+            }
+        })
+    )
+
+     passport.use(passport.serializeUser((user, done) => {
+        done(null, user.id); // or user._id depending on your DB
+        console.log(user.id)
+        }),
+    
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id); // or your user model
+            done(null, user);
+            console.log("serialized", user)
+        } catch (err) {
+            done(err);
+        }
+    }))
+}
